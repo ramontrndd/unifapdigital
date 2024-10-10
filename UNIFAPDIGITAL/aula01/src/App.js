6988d1642b7a9547266485e56b066bae7e74675a
ramontrndd <4924036@alunouniter.com>
2024-10-10 17:07:00 -0300

    Refactor App.js to remove unnecessary code and formatting

--- UNIFAPDIGITAL/aula01/src/App.js
+++ UNIFAPDIGITAL/aula01/src/App.js
@@ -1,19 +1,13 @@
 
+import React from 'react';
 import './App.css';
-import Profile from './Profile';
-import Welcome from './Welcome';
+
 
 function App() {
   return (
     <div>
-      <div  style={{display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent:'center', gap: '5px'}}> 
-        <Welcome />
-        <Profile name="João" age="25" />
-        <Profile name="Maria" age="20" />
-        <Profile name="José" age="30" />
-      </div>
-      <hr />
 
+ 
       
       </div>
 
